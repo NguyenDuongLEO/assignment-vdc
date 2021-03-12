a20092277317b885840df10bb5587994d531bf34
NguyenDuongLEO <leonguyenduong@gmail.com>
2021-03-12 18:29:09 +0700

    rebase master

--- src/main/java/manage/DriverFactory.java
+++ src/main/java/manage/DriverFactory.java
@@ -1,4 +1,14 @@
 package manage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverFactory {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://openweathermap.org/");
+        System.out.println("hello");
+        driver.close();
+    }
 }
